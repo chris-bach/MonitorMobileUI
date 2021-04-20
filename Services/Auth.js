8b21db4a3563093a314dc7ad2e10603a0ad216a1
Chris <chrisvbautista@gmail.com>
2021-04-20 19:20:11 +1000

    With Expo demo notification

--- Services/Auth.js
+++ Services/Auth.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import {MONITOR_URL} from "../constants/MonitorConstants";
 
 export const updateUser = async (data) => {
-    return await axios.put(`${MONITOR_URL}/api/user/update`, data)
+    return await axios.put(`${MONITOR_URL}/api/user/update`, {data})
 }
 
 export const login = async (data) => {
-    return await axios.post(`${MONITOR_URL}/api/login`, data)
+    console.log ("Data from service", data)
+    return await axios.post(`${MONITOR_URL}/api/login`, {data})
 }
